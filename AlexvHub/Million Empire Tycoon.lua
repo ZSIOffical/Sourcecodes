cb4ceb149854bbbf0e71dcdb7289ff087dbf6bde
ZSI <89996056+ZSIOffical@users.noreply.github.com>
2023-03-18 18:30:43 +0100

    Update Million Empire Tycoon.lua

--- AlexvHub/Million Empire Tycoon.lua
+++ AlexvHub/Million Empire Tycoon.lua
@@ -1,60 +1,78 @@
--- Made by AlexvPlays
-local library = loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/0x"))()
-local w1 = library:Window("Millionaire Empire Tycoon") -- Text
-local atfrm = false
-w1:Toggle(
+local libraryLoader = loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/0x"))()
+local window = libraryLoader:Window("Millionaire Empire Tycoon")
+local autoFarmEnabled = false
+window:Toggle(
     "Autofarm",
-    "atfrm",
+    "autoFarmEnabled",
     false,
     function(toggled)
-        atfrm = toggled
+        autoFarmEnabled = toggled
     end
 )
-w1:Button(
+window:Button(
     "Destroy GUI",
     function()
-        for i, v in pairs(game.CoreGui:GetChildren()) do
-            if v:FindFirstChild("Top") then
-                v:Destroy()
+        for i, guiElement in pairs(game.CoreGui:GetChildren()) do
+            if guiElement:FindFirstChild("Top") then
+                guiElement:Destroy()
             end
         end
         script:Destroy()
     end
-)local plrtycoon
-w1:Label("Restart script after rebirth")w1:Label("Made by AlexvPlays")repeat
-wait()for i,tcn in workspace.Tycoons:GetChildren() do
-	if tcn.Owner.Value == game.Players.LocalPlayer.Name then
-		plrtycoon = tcn
-		print("Found player's tycoon!")end
+)
+local playerTycoon
+window:Label("Restart script after rebirth")
+window:Label("Made by AlexvPlays")
+repeat
+wait()
+for i, tycoon in workspace.Tycoons:GetChildren() do
+	if tycoon.Owner.Value == game.Players.LocalPlayer.Name then
+		playerTycoon = tycoon
+		print("Found player's tycoon!")
+	end
 end
-until plrtycoon
-function money(value)
+until playerTycoon
+
+function addMoney(value)
 	local args = {
     		[1] = value}
 	game:GetService("ReplicatedStorage").RespawnService:FireServer(unpack(args))
-	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(plrtycoon.StarterParts.Collector.Givers.Giver.Position)
+	game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(playerTycoon.StarterParts.Collector.Givers.Giver.Position)
 end
+
 while true do
 	wait()
-	if atfrm == true then
+	if autoFarmEnabled == true then
 			pcall(function()
 		local buttons = {}
-		local mnnd = 0
-		for i,btnmodel in plrtycoon:WaitForChild("ButtonsFolder"):GetChildren() do
-			if not btnmodel:FindFirstChild("GamePass") and btnmodel.Name != " " then print("yes")
-				table.insert(buttons, btnmodel)
-				mnnd += btnmodel.Price.Value
-			else print("no")
+		local totalCost = 0
+
+		for i, buttonModel in playerTycoon.ButtonsFolder:GetChildren() do
+
+			if not buttonModel:FindFirstChild("GamePass") and buttonModel.Name != " " then 
+				table.insert(buttons, buttonModel)
+				totalCost += buttonModel.Price.Value
+
 			end
+
 		end
-		money(mnnd)
-		mnnd = 0
-		for i,btnmdl in buttons do
+
+		addMoney(totalCost)
+
+		for i, buttonModel in pairs(buttons) do
+			
 			wait(0.06)
-			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(btnmdl.Head.Position)
+
+			game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(buttonModel.Head.Position)
+
 			wait(0.06)
+
 		end
-		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(plrtycoon.PurchasesFolder.Rebirth6.Position)
-		end)
-	end
+		
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(playerTycoon.PurchasesFolder.Rebirth6.Position)
+
+	end)
+
+end
+
 end
